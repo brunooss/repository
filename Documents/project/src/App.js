af5de6b3bfdc7184b67a3f34dd983643ce2d9985
brunooss <bruno.oss@outlook.com>
2018-08-21 20:12:49 -0300

    v1

---
+++ Documents/project/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Counter from "./components/counter"
+import "./styles/bootstrap.css"
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Counter/>
+        );
+    }
+}
