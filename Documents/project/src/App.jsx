dd191c00241c19c4d7ebc9da4352f48cc5f7121e
brunooss <bruno.oss@outlook.com>
2018-08-24 14:15:39 -0300

    test commit 01

---
+++ Documents/project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import React, { Component } from "react";
+
+class Component extends Component {
+  state = {};
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+export default Component;
